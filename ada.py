12e67e7fcf8e1f0f195e4109148c8d24c0cb4002
marcosmassone <mmassone@mail.austral.edu.ar>
2025-03-31 15:26:05 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -4,7 +4,6 @@ def ada():
     total_lower = first_name.lower() + " " + last_name.lower()
     title_case = first_name.capitalize() + " " + last_name.capitalize()
     total_upper = first_name.upper() + " " + last_name.upper()
-    tabbed_lower = "\t" + total_lower
     print(total_lower)
     print(title_case)
     print(total_upper)    
